9a66c1e3f6a51ee73bad61839e401acf7a26fcda
saplyng <saplyng@gmail.com>
2019-09-10 16:51:25 -0500

    working buttons for tic tac toe, no logic

--- tictactoe/ViewController.swift
+++ tictactoe/ViewController.swift
@@ -23,117 +23,119 @@ class ViewController: UIViewController {
     @IBOutlet weak var square9: UIButton!
     
     
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     }
 
     
-    @IBAction func square1switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+    
+    @IBAction func square1switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
         }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
     }
     
-    @IBAction func square2switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+    @IBAction func square2switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
         }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
-        }
-    }
-    
-    @IBAction func square4switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
-        }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
-        }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
     }
-    
-    @IBAction func square42switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+//
+    @IBAction func square4switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+        sender.setImage(UIImage(named:"x"),for:[])
         }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
+        else if sender.image(for:[])==UIImage(named:"x"){
+        sender.setImage(UIImage(named:"o"),for:[])
         }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
-        }
-    }
-    
-    @IBAction func square5switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
-        }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
-        }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+        else if sender.image(for:[])==UIImage(named:"o"){
+        sender.setImage(UIImage(named:"x"),for:[])
         }
     }
-    
-    @IBAction func square6switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+
+    @IBAction func square42switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
         }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
-        }
-    }
-    
-    @IBAction func square7switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
-        }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
-        }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
     }
-    
-    @IBAction func square8switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+
+    @IBAction func square5switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
         }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
     }
-    
-    @IBAction func square9switch(_ sender: Any) {
-        if (UIImage(named:"transparent") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+
+    @IBAction func square6switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
-        else if (UIImage(named:"x") != nil){
-            square5.setImage(UIImage(named:"o"),for:[])
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
         }
-        else if (UIImage(named:"o") != nil){
-            square5.setImage(UIImage(named:"x"), for: [])
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
+        }
+    }
+
+    @IBAction func square7switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
+        }
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
+        }
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
+        }
+    }
+
+    @IBAction func square8switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
+        }
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
+        }
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
+        }
+    }
+
+    @IBAction func square9switch(_ sender: UIButton) {
+        if sender.image(for:[])==UIImage(named:"transparent"){
+            sender.setImage(UIImage(named:"x"),for:[])
+        }
+        else if sender.image(for:[])==UIImage(named:"x"){
+            sender.setImage(UIImage(named:"o"),for:[])
+        }
+        else if sender.image(for:[])==UIImage(named:"o"){
+            sender.setImage(UIImage(named:"x"),for:[])
         }
     }
 }
